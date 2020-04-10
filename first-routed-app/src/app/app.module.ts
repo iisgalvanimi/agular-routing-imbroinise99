f33e87db1cb332b77963ab5e7515ff681d6a5839
imbroinise99 <imbroinise.davide@iisgalvanimi.edu.it>
2020-04-10 14:47:53 +0000

    01.1

--- first-routed-app/src/app/app.module.ts
+++ first-routed-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+import { TrackComponent } from './track/track.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    AlbumComponent,
+    ArtistComponent,
+    TrackComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
